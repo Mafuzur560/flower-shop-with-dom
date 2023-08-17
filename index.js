d29a138a0ce80b9c4530621f30f0cf3081b4f330
Mafuzur560 <mrridoy461@gmail.com>
2023-08-17 20:06:25 +0600

    grand discount complited

--- index.js
+++ index.js
@@ -16,6 +16,20 @@ function handleBtn(o){
 
     // total add
     document.getElementById('add-total').innerText = total;
-    
+
+    // bounaces
+    const grandPrice = document.getElementById('grand-price');
+
+    // discount
+    const discount = document.getElementById('price-discound');
+    const discoundNumber = parseFloat(discount);
+    if(total >= 50){
+        discount.innerText = '15';
+        const discountAmount = (total * 15) / 100;
+        const discountedPrice = total - discountAmount;
+        grandPrice.innerText = discountedPrice ;
+    }else{
+        grandPrice.innerText = total ;
+    }
     
 }
